e744c6a4c1d7bff957c2df6fcf7ef3acb99f1998
manenim udoh <manenimabasi.udoh@stu.cu.edu.ng>
2024-05-26 12:57:37 +0100

    implement header bug

--- src/components/homeHeader.tsx
+++ src/components/homeHeader.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import Link from "next/link";
 
 const HomeHeader = () => {
   return (
     <div className="bg-[#051632] shadow-lg">
       <div className="w-full h-[9vh] px-16 flex justify-between items-center">
-        <div className="logo">
-          <h2 className="text-2xl font-bold text-white">School Mgt.</h2>
-        </div>
+        <Link href="/">
+          <div className="logo cursor-pointer">
+            <h2 className="text-2xl font-bold text-white">School Mgt.</h2>
+          </div>
+        </Link>
         <Avatar>
           <AvatarImage src="https://github.com/shadcn.png" />
           <AvatarFallback>CN</AvatarFallback>
