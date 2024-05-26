4d632081cd066b13e99d9eff75a48d213ada17b9
manenim udoh <manenimabasi.udoh@stu.cu.edu.ng>
2024-05-26 12:36:10 +0100

    implement header bug

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -4,7 +4,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 const Header = () => {
   return (
-    <div className='800 shadow-lg'>
+    <div className='shadow-lg bg-white z-[50]'>
       <div className="w-full h-[9vh] px-16 flex justify-between items-center">
         <div className="logo">
           <p className="text-2xl font-bold">Dashboard</p>
