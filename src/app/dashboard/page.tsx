112f279196a7bda85147c7cba6dd8cb060b2e5f2
manenim udoh <manenimabasi.udoh@stu.cu.edu.ng>
2024-05-26 13:41:01 +0100

    fix sidebar styling

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -16,7 +16,7 @@ const Display = () => {
     <div>
       <h3 className="font-extrabold text-5xl px-12 py-16">Overview</h3>
 
-      <div className="flex gap-20 px-12 mt-12 ml-[6%]">
+      <div className="flex gap-14 px-12 mt-12 ml-[6%]">
         {!teachers ? (
           <div>
             <SkeletonCard />
@@ -28,9 +28,9 @@ const Display = () => {
                 <SkeletonCard />
               </div>
             }>
-            <div className="card cursor-pointer shadow-lg bg-white border rounded-lg w-[40%] min-h-[38vh]">
+            <div className="card cursor-pointer shadow-lg bg-white border rounded-lg w-[34%] min-h-[38vh]">
               <Link href="/dashboard/teachers">
-              <h3 className="font-bold text-[1.8rem] pt-8 pl-4">
+              <h3 className="font-bold text-[1.4rem] pt-8 pl-4">
                 Total Number of Teachers
               </h3>
               <h3 className="font-bolder text-[8rem] pl-[6rem] pt-6">{teachers.length}</h3>
@@ -50,10 +50,10 @@ const Display = () => {
               </div>
             }>
            
-              <div className="card shadow-lg cursor-pointer bg-white border rounded-lg w-[40%] min-h-[38vh]">
+              <div className="card shadow-lg cursor-pointer bg-white border rounded-lg w-[34%] min-h-[38vh]">
                               <Link href="/dashboard/students">
 
-          <h3 className="font-bold text-[1.8rem] pt-8 pl-4">
+          <h3 className="font-bold text-[1.4rem] pt-8 pl-4">
             Total Number of Students
           </h3>
                   <h3 className="font-bolder text-[8rem] pl-[6rem] pt-6">{students.length}</h3>
