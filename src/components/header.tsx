21a5ead1057bdce046b82aa8069b7ad7ea5846fb
manenim udoh <manenimabasi.udoh@stu.cu.edu.ng>
2024-05-26 12:56:06 +0100

    implement header bug

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,15 @@
-import React from 'react'
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
-
+import Link from "next/link";
 
 const Header = () => {
   return (
-    <div className='shadow-lg bg-white z-[50]'>
+    <div className="shadow-lg bg-white z-[50]">
       <div className="w-full h-[9vh] px-16 flex justify-between items-center">
-        <div className="logo">
-          <p className="text-2xl font-bold">Dashboard</p>
-        </div>
+        <Link href="/">
+          <div className="logo cursor-pointer">
+            <p className="text-2xl font-bold">School Mgt.</p>
+          </div>
+        </Link>
         <Avatar>
           <AvatarImage src="https://github.com/shadcn.png" />
           <AvatarFallback>CN</AvatarFallback>
@@ -16,6 +17,6 @@ const Header = () => {
       </div>
     </div>
   );
-}
+};
 
-export default Header
+export default Header;
--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -15,7 +15,7 @@ const Sidebar = () => {
           <h3 className="font-bold text-white py-4">ADMIN</h3>
         </div>
       </Link>
-      <div className="mt-[6rem] text-white text-bold text-xl">
+      <div className="mt-[6rem] text-white text-xl">
         <Link href="/dashboard/teachers">
           <div
             className={`flex items-center mb-12 pl-8 py-3 rounded-tr-3xl rounded-br-3xl text-white ${
